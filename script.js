2732c1723cb26d8d7de8d3725f5ca6c8cf8e4e72
elosouza09 <00001115444980sp@aluno.educacao.sp.gov.br>
2025-09-01 08:28:26 -0300

    a

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+
+    botaoDeAcessibilidade.addEventListener('click', function () {
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+
+    })
+
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-    const diminuirFonteBotao = document.getElementById('diminuir-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
-        let tamanhoAtualFonte = 1;
+    const alternaContraste = document.getElementById('alterna-contraste')
 
-    aumentaFonteBotao.addEventListener('click', function(){
+    let tamanhoAtualFonte = 1;
+
+    aumentaFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
     })
 
-    diminuirFonteBotao.addEventListener('click', function(){
+    diminuiFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
     })
 
-    const botaoAcessibilidade = document.getElementById('botao-acessibilidade');
-    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
-    const alternaContraste = document.getElementById('alterna-contraste');
-
-    botaoAcessibilidade.addEventListener('click', function(){
-        botaoAcessibilidade.classList.toggle('rotacao-botao');
-         opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
-    })
-
-    alternaContraste.addEventListener('click', function(){
+    alternaContraste.addEventListener('click', function () {
         document.body.classList.toggle('alto-contraste')
     })
+
 
 })
